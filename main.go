7c08382679ef1cfec376114f1fb95cac07f556dd
pwh-pwh <plmoknpwh@163.com>
2023-12-14 15:16:51 +0800

    1.Splitting out our Websocket Code

--- main.go
+++ main.go
@@ -2,52 +2,25 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/websocket"
-	"log"
+	"github.com/pwh-pwh/chat-app/pkg/websocket"
 	"net/http"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		fmt.Println(string(p))
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrade(w, r)
 	if err != nil {
-		log.Println(err)
+		fmt.Fprintf(w, "%+V\n", err)
 	}
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func setupRoutes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
 	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
-	fmt.Println("Chat App v0.0.1")
+	fmt.Println("Chat App v0.01")
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
 }
---
+++ pkg/websocket/websocket.go
@@ -0,0 +1,65 @@
+package websocket
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"io"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return ws, err
+	}
+	return ws, nil
+}
+
+func Reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println(string(p) + " from reader")
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func Writer(conn *websocket.Conn) {
+	for {
+		fmt.Println("Sending")
+		messageType, r, err := conn.NextReader()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		w, err := conn.NextWriter(messageType)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if _, err := io.Copy(w, r); err != nil {
+			log.Println(err)
+			return
+		}
+		if err := w.Close(); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
